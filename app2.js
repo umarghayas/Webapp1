1fb2abf56e8c96d6e74e069141184d131b56dff5
Umar Ghayas <umar.ghayas2000@gmail.com>
2023-11-23 18:48:55 +0500

    Complete Stopwatch and Todo List (Responsive)

--- app2.js
+++ app2.js
@@ -9,28 +9,54 @@ var secCount = 0;
 var msCount = 0;
 var interval;
 // console.log(mins.innerHTML, sec.innerHTML, ms.innerHTML)
-function startTimer(ele){
-    console.log(ele)
+function startTimer(ele) {
+    console.log(ele.nextElementSibling)
+    ele.nextElementSibling.disabled = false
+    ele.nextElementSibling.nextElementSibling.disabled = false
+
     // ele.setAttribute("disabled","true")
     ele.disabled = true
-    interval = setInterval(function (){
+    interval = setInterval(function () {
         msCount++;
         ms.innerHTML = msCount;
-        if(msCount == 100){
+        if (msCount == 100) {
             secCount++;
             msCount = 0;
             ms.innerHTML = msCount;
             sec.innerHTML = secCount;
+            if(secCount == 60){
+                minsCount++;
+                secCount = 0;
+                mins.innerHTML = minsCount;
+                sec.innerHTML = secCount
+                if(minsCount == 60){
+                    hoursCount++;
+                    minsCount = 0;
+                    hours.innerHTML = hoursCount
+                    mins.innerHTML = minsCount
+                }
+            }
         }
     }, 10)
 
 }
-function stopTimer(ele){
+function stopTimer(ele) {
     clearInterval(interval)
     console.log(ele.previousElementSibling)
-    ele.previousElementSibling = 
+    ele.previousElementSibling.disabled = false
     // ele.previousElementSibling.setAttribute("disabled","false")
 }
-function Reset(ele){
-
+function Reset(ele) {
+    clearInterval(interval)
+    ele.previousElementSibling.previousElementSibling.disabled = false
+    ele.disabled = true
+    ele.previousElementSibling.disabled = true
+    hoursCount = 0;
+    minsCount = 0;
+    secCount = 0;
+    msCount = 0;
+    hours.innerHTML = "00"
+    mins.innerHTML = "00"
+    sec.innerHTML = "00"
+    ms.innerHTML = "00"
 }
